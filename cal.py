db0130faaa1432622da6d72ca9737917f5ba510a
sneha365-star <kattupallisnehasri@gmail.com>
2026-01-05 14:38:01 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
